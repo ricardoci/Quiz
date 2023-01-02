ea49ca2ba5ac48471a036b0db14b7719bdafa2b8
Ricardo Cisneros <ricardocisneros197@gmail.com>
2023-01-01 21:28:13 -0800

    created a randomizer for array of questions

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,43 @@
-var questions = {
+var questions = [{
+    
+    
     
     numb: 1,
     question: "what is html?",
-    answer: "correct",
-    options: [ "no", "no","no","no"]
+    answer: "yes",
+    options: [ "yes", "no","no","no"]
+},
 
-} 
-console.log(questions.numb)
+
+
+{   numb: 2,
+    question: "what is css?",
+    answer: "yes",
+    options: [ "no", "no","no","yes"]
+
+
+    
+
+}]
+// the findIndex method allows for me to acces specific propeties from an object array.
+var result = questions.findIndex(item => item.question === 2);
+    
+
+
+var r = ([questions[0].question, questions[1].question]);
+
+
+//this question randomizes questions
+function random(arr){
+  
+    var rp = Math.floor(Math.random() * arr.length);
+    var pr = arr[rp];
+  
+     return pr;
+  }
+console.log(random(r));
+
+
+
+
 
