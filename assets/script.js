514dbcdc9027705688d9cd46f4ed5f9bf3d1390d
ricar <ricardocisneros197@gmail.com>
2023-01-04 18:26:52 -0800

    added a counter so that each time I click next, it adds 1 to my index. That changes the indexes and gives me my next array object

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
+// event selectors
+var playbutton = $('#play-button');
+var quizbox = $('.quizBox');
+var box = $('.box')
+var exitButton = $('#exitButton')
+var nextQuestion = $('#nextQuestion')
+
+
+
+
+
 var questions = [{
     
     
@@ -5,7 +16,7 @@ var questions = [{
     numb: 1,
     question: "what is html?",
     answer: "yes",
-    options: [ "yes", "no","no","no"]
+    options: [ "1", "no","no","1"]
 },
 
 
@@ -13,7 +24,7 @@ var questions = [{
 {   numb: 2,
     question: "what is css?",
     answer: "yes",
-    options: [ "no", "no","no","yes"]
+    options: [ "2", "no","no","2"]
 
 
     
@@ -22,22 +33,119 @@ var questions = [{
 {   numb: 3,
     question: "what is git?",
     answer: "yes",
-    options: [ "no", "no","no","yes"]
+    options: [ "3", "no","no","3"]
 
 
     
 
 }];
-// the findIndex method allows for me to acces specific propeties from an object array.
-var result = questions.findIndex(item => item.question === 2);
+
+
+
+ 
+
+  
+
+
+// jquery adding questions 
+
+var question = $('.question');
+question.children('span').text(questions[0].question);
+
+// jquery adding the number question you are on
+var questionsNum = $('.questionsNum');
+questionsNum.children('h1').children('p').text(questions[0].numb);
+
+// jquery answers
+var answers = $('.answers');
+answers.children('option').children('h1').text(questions[0].options[0]);
+
+var answers = $('.answers');
+answers.children('option').children('h2').text(questions[0].options[1]);
+
+
+var answers = $('.answers');
+answers.children('option').children('h3').text(questions[0].options[2]);
+
+
+var answers = $('.answers');
+answers.children('option').children('h4').text(questions[0].options[3]);
+
+
+//event listeners
+// play button
+playbutton.on('click', function(){
+
+    if(playbutton){
+
+   
+box.css('display', "none")
+
+quizbox.css('display', "block ")
+
+
+    }  
+});
+
+
+// exitButton
+exitButton.on('click', function(){
+
+    if(exitButton){
+
+   
+box.css('display', "block")
+
+quizbox.css('display', "none ")
+
+
+    }  
+});
+
+// next button
+var index = 1;
+
+
+
+nextQuestion.on('click', function(){
+
     
 
+    if(nextQuestion){
+       
+       
+        var question = $('.question');
+        question.children('span').text(questions[index].question);
 
-var r = [questions[0].question, questions[1].question,questions[2].question];
+        var questionsNum = $('.questionsNum');
+        questionsNum.children('h1').children('p').text(questions[index].numb);
+        
+        // jquery answers
+        var answers = $('.answers');
+        answers.children('option').children('h1').text(questions[index].options[0]);
+        
+        var answers = $('.answers');
+        answers.children('option').children('h2').text(questions[index].options[1]);
+        
+        
+        var answers = $('.answers');
+        answers.children('option').children('h3').text(questions[index].options[2]);
+        
+        
+        var answers = $('.answers');
+        answers.children('option').children('h4').text(questions[index].options[3]);
 
+        
 
-//this question randomizes questions
+    }  
+    // adds 1 to the index
+    if(question.length === index){
+        index++;
+        console.log(index)
+    }
+   
+    
+});
 
-console.log(r[0],r[2])
 
 
