237506a245add1d5baec721e83428791ea84e585
ricar <ricardocisneros197@gmail.com>
2023-01-06 21:07:38 -0800

    question quiz done. next to add timer and score

--- assets/script.js
+++ assets/script.js
@@ -1,225 +1,215 @@
-// event selectors
-var playbutton = $('#play-button');
-var quizbox = $('.quizBox');
-var box = $('.box')
-var box2 = $('.box2')
-var exitButton = $('#exitButton')
-var nextQuestion = $('#nextQuestion')
-let question = $('.question');
-
-
-
-
-var questions = [{
-    
-    
-    
-    numb: 1,
-    question: "what is html?",
-    answer: "yes",
-    options: [ "1", "no","no","yes"]
-},
-
-
-
-{   numb: 2,
-    question: "what is css?",
-    answer: "yes",
-    options: [ "2", "no","no","2"]
-
-
-    
+const Questions = [{
+    id: 0,
+    q: "What is html?",
+    a: [{ text: "makr u", isCorrect: false },
+        { text: "Surat", isCorrect: false },
+        { text: "Delhi", isCorrect: true },
+        { text: "mumbai", isCorrect: false }
+    ]
 
 },
-{   numb: 3,
-    question: "what is git?",
-    answer: "yes",
-    options: [ "3", "no","no","3"]
+{
+    id: 1,
+    q: "What is css?",
+    a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+        { text: "phuket", isCorrect: false },
+        { text: "Ayutthaya", isCorrect: false },
+        { text: "Bangkok", isCorrect: true }
+    ]
 
 },
-   
-{   numb: 4,
-        question: "what is version control?",
-        answer: "yes",
-        options: [ "4", "no","no","4"]
-    
-    
-    
-},  
-{   numb: 5,
-        question: "what is code?",
-        answer: "yes",
-        options: [ "5", "no","no","5"]
-    
-}, 
-{   numb: 6,
-    question: "what is code?",
-    answer: "yes",
-    options: [ "5", "no","no","5"]
+{
+    id: 2,
+    q: "What is javascript?",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
+    ]
 
-}, 
-{   numb: 7,
-    question: "what is code?",
-    answer: "yes",
-    options: [ "5", "no","no","5"]
+},
+{
+    id: 3,
+    q: "What is an object?",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
+    ]
 
-}, 
-{   numb: 8,
-    question: "what is code?",
-    answer: "yes",
-    options: [ "5", "no","no","5"]
+},
+{
+    id: 4,
+    q: "What is array?",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
+    ]
 
-}, 
-{   numb: 9,
-    question: "what is code?",
-    answer: "yes",
-    options: [ "5", "no","no","5"]
+},
+{
+    id: 5,
+    q: "What is boolean?",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
+    ]
 
-}, 
-{   numb: 10,
-    question: "wha?",
-    answer: "yes",
-    options: [ "5", "no","no","5"]
+},
+{
+    id: 6,
+    q: "What is local storage?",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
+    ]
 
- 
-    
-    
+},
+{
+    id: 7,
+    q: "What is innertext?",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
+    ]
+
+},{
+    id: 8,
+    q: "What is a function?",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
+    ]
+
+},
+{
+    id: 9,
+    q: "what is a method?",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
+    ]
+
+},
+{
+    id: 10,
+    q: "what bootstrap?",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
+    ]
+
+}
+
+
+]
+
+var start = true;
   
+// Iterate
+function iterate(id) {
+  
+    // Getting the result display section
+    var result = document.getElementsByClassName("question");
+    result[0].innerText = "";
+  
+    // Getting the question
+    const question = document.getElementById("result");
+  
+  
+    // Setting the question text
+    question.innerText = Questions[id].q;
+  
+    // Getting the options
+    const op1 = document.getElementById('op1');
+    const op2 = document.getElementById('op2');
+    const op3 = document.getElementById('op3');
+    const op4 = document.getElementById('op4');
+  
+  
+    // Providing option text 
+    op1.innerText = Questions[id].a[0].text;
+    op2.innerText = Questions[id].a[1].text;
+    op3.innerText = Questions[id].a[2].text;
+    op4.innerText = Questions[id].a[3].text;
+  
+    // Providing the true or false value to the options
+    op1.value = Questions[id].a[0].isCorrect;
+    op2.value = Questions[id].a[1].isCorrect;
+    op3.value = Questions[id].a[2].isCorrect;
+    op4.value = Questions[id].a[3].isCorrect;
+  
+    var selected = "";
+  
+    // Show selection for op1
+    op1.addEventListener("click", () => {
+        
+        selected = op1.value;
+    })
+  
+    // Show selection for op2
+    op2.addEventListener("click", () => {
+        
+        selected = op2.value;
+    })
+  
+    // Show selection for op3
+    op3.addEventListener("click", () => {
+        
+        selected = op3.value;
+    })
+  
+    // Show selection for op4
+    op4.addEventListener("click", () => {
        
-
-}];
-
-
-
-
-
- 
-
+        selected = op4.value;
+    })
   
-
-
-// jquery adding questions 
-
- question = $('.question');
-question.children('span').text(questions[0].question);
-
-// jquery adding the number question you are on
+    // Grabbing the evaluate button
+    const evaluate = document.getElementsByClassName("exitButton");
+  
+    // Evaluate method
+    evaluate[0].addEventListener("click", () => {
+        if (selected == "true") {
+            result[0].innerHTML = "True";
+            result[0].style.backgroundColor = "green";
+        } else {
+            result[0].innerHTML = "False";
+            result[0].style.backgroundColor = "red";
+        }
+    })
+}
+  
+if (start) {
+    iterate("0");
+}
+  
+// Next button and method
+const next = document.getElementsByClassName('nextQuestion')[0];
+var id = 1;
 var questionsNum = $('.questionsNum');
-questionsNum.children('h1').children('p').text(questions[0].numb);
-
-// jquery answers
-var answers = $('.answers');
-answers.children('option').children('h1').text(questions[0].options[0]);
-
-var answers = $('.answers');
-answers.children('option').children('h2').text(questions[0].options[1]);
-
-
-var answers = $('.answers');
-answers.children('option').children('h3').text(questions[0].options[2]);
-
-
-var answers = $('.answers');
-answers.children('option').children('h4').text(questions[0].options[3]);
-
-
-//event listeners
-// play button
-playbutton.on('click', function(){
-
-    if(playbutton){
-
-   
-box.css('display', "none")
-
-quizbox.css('display', "block ")
-
-
-    }  
-});
-
-
-// exitButton
-exitButton.on('click', function(){
-
-    if(exitButton){
-
-   
-box.css('display', "block")
-
-quizbox.css('display', "none ")
-
-
-    }  
-});
-// stops quiz at question 10.hides quiz and presents you with your score
-nextQuestion.on('click', function(){
-
-    if( index === 10  ){
-
-        
-        box2.css('display', "block")
-        quizbox.css('display', "none ")
-    } 
-});
-
-// next button
-let index = 1;
-
-
-
-nextQuestion.on('click', function(){
-
-    
-
-    if(nextQuestion){
-       
-       
-        
-        question.children('span').text(questions[index].question);
-
-        var questionsNum = $('.questionsNum');
-        questionsNum.children('h1').children('p').text(questions[index].numb);
-        
-        // jquery answers
-        var answers = $('.answers');
-        answers.children('option').children('h1').text(questions[index].options[0]);
-        
-        var answers = $('.answers');
-        answers.children('option').children('h2').text(questions[index].options[1]);
-        
-        
-        var answers = $('.answers');
-        answers.children('option').children('h3').text(questions[index].options[2]);
-        
-        
-        var answers = $('.answers');
-        answers.children('option').children('h4').text(questions[index].options[3]);
-
-       
-
-       
-     console.log(questions[index].numb )
-     console.log(index)
-
-    }  
-    // adds 1 to the index, if question.length === index is false, next button will not add
-    if( questions[index].numb < 11){
-        index++;
-        
-        console.log('dkdk')
-    }
-   
-   
 
   
-   
-
-   
+next.addEventListener("click", () => {
     
-   
-    
-});
-
-
+    start = false;
+    if (id < 10) {
+        
+            id++
+            questionsNum.children('h1').children('p').text(id);
+      
+        
+        
+        iterate(id);
+        console.log(id);
+    }
+  
+})
 
